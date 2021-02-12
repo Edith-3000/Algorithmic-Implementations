0ddc7a5e034fd89df388489ad0690e3edbd365b0
Kapil Choudhary <kapil181112309@gmail.com>
2021-02-12 23:07:35 +0530

    Files UPD

---
+++ Constructive Algorithms/Miscellaneous/Problem - 1.cpp
@@ -0,0 +1,89 @@
+// Reference: https://codeforces.com/blog/entry/17612
+//            https://www.geeksforgeeks.org/represent-number-sum-minimum-possible-psuedobinary-numbers/
+/********************************************************************************************************/
+
+// Problem: B. Quasi Binary
+// Contest: Codeforces - Codeforces Round #300
+// URL: https://codeforces.com/problemset/problem/538/B
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// Parsed on: 12-02-2021 12:03:45 IST (UTC+05:30)
+// Author: Kapil Choudhary
+// ********************************************************************
+// कर्मण्येवाधिकारस्ते मा फलेषु कदाचन |
+// मा कर्मफलहेतुर्भूर्मा ते सङ्गोऽस्त्वकर्मणि || १.४७ ||
+// ********************************************************************
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << ", " << #y << "=" << y << endl
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int>	vi;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<pii>	vpii;
+typedef vector<pll>	vpll;
+typedef vector<vi> vvi;
+typedef vector<vll>	vvll;
+typedef vector<vull> vvull;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+    uniform_int_distribution<int> uid(0,lim-1);
+    return uid(rang);
+}
+
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+
+void solve()
+{
+  	int n; cin >> n;
+  	vi res;
+  	
+  	while(n) {
+  		int tmp = n, m = 0, p = 1;
+  		while(tmp) {
+  			int rem = tmp % 10;
+  			if(rem != 0) m += p;
+  			
+  			tmp /= 10;
+  			p *= 10;
+  		}
+  		
+  		res.pb(m);
+  		n -= m;
+  	}
+  	
+  	cout << res.size() << "\n";
+  	for(auto x: res) cout << x << " ";
+  	cout << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+      solve();
+    }
+
+    return 0;
+}
