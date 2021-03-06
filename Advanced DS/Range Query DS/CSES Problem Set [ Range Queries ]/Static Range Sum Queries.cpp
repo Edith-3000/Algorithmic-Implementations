956d533da07ffe455132be7d82c56b277f0c45dc
Kapil Choudhary <kapil181112309@gmail.com>
2021-03-06 22:39:35 +0530

    Files updated

---
+++ Advanced DS/Range Query DS/CSES Problem Set [ Range Queries ]/Static Range Sum Queries.cpp
@@ -0,0 +1,80 @@
+// Problem: Static Range Sum Queries
+// Contest: CSES - CSES Problem Set
+// URL: https://cses.fi/problemset/task/1646
+// Memory Limit: 512 MB
+// Time Limit: 1000 ms
+// Parsed on: 06-03-2021 13:42:16 IST (UTC+05:30)
+// Author: Kapil Choudhary
+// ********************************************************************
+// कर्मण्येवाधिकारस्ते मा फलेषु कदाचन |
+// मा कर्मफलहेतुर्भूर्मा ते सङ्गोऽस्त्वकर्मणि || १.४७ ||
+// ********************************************************************
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << ", " << #y << "=" << y << endl
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int>	vi;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<pii>	vpii;
+typedef vector<pll>	vpll;
+typedef vector<vi> vvi;
+typedef vector<vll>	vvll;
+typedef vector<vull> vvull;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+    uniform_int_distribution<int> uid(0,lim-1);
+    return uid(rang);
+}
+
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+
+void solve()
+{
+  	int n, q; cin >> n >> q;
+  	vi v(n);
+  	for(int i = 0; i < n; i++) cin >> v[i];
+  	
+  	vll pref(n, 0);
+  	for(int i = 0; i < n; i++) {
+  		pref[i] = v[i];
+  		if(i) pref[i] += pref[i-1];
+  	}
+  	
+  	while(q--) {
+  		int L, R; cin >> L >> R;
+  		L--, R--;
+  		cout << pref[R] - (L ? pref[L-1] : 0LL) << "\n";
+  	}
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+      solve();
+    }
+
+    return 0;
+}
---
+++ Advanced DS/Range Query DS/Segment Tree/(Not so) General Purpose Segment Tree Library - Kartik Arora.cpp
@@ -0,0 +1,147 @@
+/* # Ref: https://github.com/kartik8800/segTree
+          https://github.com/kartik8800/segTree/blob/master/SegmentTree.h
+          https://codeforces.com/blog/entry/71101
+          https://www.youtube.com/watch?v=K-86mKNAsmU
+*/
+
+// This template can be used to solve problems involving "Point updates and Range queries", the 
+// updates work in log2(n) time and build works in O(n) time.
+// WARNING: This is not a robustly tested library (however, easier segment tree problems can be 
+//          solved by it pretty well.)
+
+/*****************************************************************************************************************/
+
+///author : kartik8800
+#ifndef SEGMENTTREE_H
+#define SEGMENTTREE_H
+#define left(i) (2*i + 1)
+#define right(i) (2*i + 2)
+#define parent(i) ((i-1)/2)
+#include <vector>
+
+template<class T>
+class SegmentTree
+{
+    public:
+        //tree constructors.
+        SegmentTree(std::vector<T> data, T value, T (*combine)(T obj1, T obj2));
+        SegmentTree(T ar[], int n, T value, T (*combine)(T obj1, T obj2));
+        
+        //query the range l to r, 0 based array indexing.
+        T query(int l, int r);
+        
+        //update the element at index idx to val.
+        void update(int idx, T val);
+        ///TODO lazy propagation
+    private:
+        //represents the segment tree.
+        T *tree;
+    
+        //builds the segment tree.
+        void buildTree(std::vector<T> data);
+        
+        //size of the segment tree array.
+        int segTreeSize;
+    
+        //extra nodes must be added to array to make its size a power of 2
+        //this is the value to be filled for the those nodes.
+        T valueForExtraNodes;
+    
+        //specifies how to combine child node results to form parent node result.
+        T (*combine)(T obj1, T obj2);
+    
+        //used to calculate the size of array needed to store the tree.
+        int calculateSize(int n);
+    
+        //helps to solve a range query.
+        T queryHelper(int l,int r, int st, int ed, int node);
+};
+
+template<class T> SegmentTree<T>::SegmentTree(std::vector<T> data,
+                                                T value, T (*combine)(T obj1, T obj2))
+{
+   this->combine = combine;
+   valueForExtraNodes = value;
+   segTreeSize = calculateSize(data.size());
+   buildTree(data);
+}
+
+template<class T> SegmentTree<T>::SegmentTree(T ar[], int n,
+                                            T value, T (*combine)(T obj1, T obj2))
+{
+   this->combine = combine;
+   valueForExtraNodes = value;
+   segTreeSize = calculateSize(n);
+
+   std::vector<T> data;
+   for(int i = 0; i < n; i++)
+         data.push_back(ar[i]);
+
+   buildTree(data);
+}
+
+
+template<class T> int SegmentTree<T>::calculateSize(int n)
+{
+    int pow2 = 1;
+    while( pow2 < n)
+    {
+        pow2 = pow2 << 1;
+    }
+    return 2*pow2 - 1;
+}
+
+template<class T> T SegmentTree<T>::query(int l, int r)
+{
+    int st = 0, ed = segTreeSize/2;
+    return queryHelper(l, r, st, ed, 0);
+}
+
+template<class T> T SegmentTree<T>::queryHelper(int l,int r, int st, int ed, int node)
+{
+    if( (r < st) || (l > ed) || (l > r) )
+        return valueForExtraNodes;
+    if(st >= l && ed <= r)
+        return tree[node];
+    T leftVal = queryHelper(l, r, st, (st + ed)/2, left(node));
+    T rightVal = queryHelper(l, r, (st+ed)/2 + 1, ed, right(node));
+    return combine(leftVal, rightVal);
+}
+
+template<class T> void SegmentTree<T>::buildTree(std::vector<T> data)
+{
+   int n = data.size();
+   tree = new T[segTreeSize];
+   int extraNodes = (segTreeSize/2 + 1) - n;
+   for(int i = segTreeSize - 1; i >= 0; i--)
+   {
+       if(extraNodes>0)
+           {
+               tree[i] = valueForExtraNodes;
+               extraNodes--;
+           }
+       else if(n>0)
+           {
+               tree[i] = data[n-1];
+               n--;
+           }
+       else
+           tree[i] = combine(tree[left(i)], tree[right(i)]);
+   }
+}
+
+template<class T> void SegmentTree<T>::update(int idx, T val)
+{
+    int segTreeIdx = (segTreeSize/2) + idx;
+    tree[segTreeIdx] = val;
+    while(parent(segTreeIdx) >= 0)
+    {
+        segTreeIdx = parent(segTreeIdx);
+        if(right(segTreeIdx) < segTreeSize)
+          tree[segTreeIdx] = combine(tree[left(segTreeIdx)], tree[right(segTreeIdx)]);
+        if(segTreeIdx == 0)
+            break;
+    }
+}
+
+#endif // SEGMENTTREE_H
