7dca7d17dcad9d071d32db9f8081ea259edf3f62
Kapil Choudhary <kapil181112309@gmail.com>
2021-05-07 08:43:48 +0530

    Files UPD

---
+++ Stack/Minimum Parantheses To Make a String Valid.cpp
@@ -0,0 +1,76 @@
+// Problem: https://www.interviewbit.com/problems/minimum-parantheses/
+/***************************************************************************************************/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << ", " << #y << "=" << y << endl
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int>	vi;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<pii>	vpii;
+typedef vector<pll>	vpll;
+typedef vector<vi> vvi;
+typedef vector<vll>	vvll;
+typedef vector<vull> vvull;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+    uniform_int_distribution<int> uid(0,lim-1);
+    return uid(rang);
+}
+
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+
+int min_paren(string &s) {
+	int n = (int)s.size();
+	int cnt = 0, res = 0;
+	
+	for(int i = 0; i < n; i++) {
+        if(s[i] == '(') cnt++;
+        else {
+            if(cnt <= 0) res++;
+            else cnt--;
+        }
+    }
+    
+    res += cnt;
+    return res;
+}
+
+void solve()
+{
+  	string s; cin >> s;
+  	cout << min_paren(s) << "\n";  	
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+
+    int t = 1;
+    // int test = 1;
+    // cin >> t;
+    while(t--) {
+      // cout << "Case #" << test++ << ": ";
+      solve();
+    }
+
+    return 0;
+}
