32c46e5da4fa56712b17d738dcdb0f284b426360
Kapil Choudhary <kapil181112309@gmail.com>
2021-05-24 23:38:42 +0530

    Files UPD

--- Strings/Substrings or Subsequence Related Problems/Subsequence Related Concepts/Count Distinct Subsequences.cpp
+++ Strings/Substrings or Subsequence Related Problems/Subsequence Related Concepts/Count Distinct Subsequences.cpp
@@ -8,24 +8,62 @@
 using namespace std;
 
 #define ll long long
+#define ld long double
 #define ull unsigned long long
 #define pb push_back
+#define ppb pop_back
 #define mp make_pair
 #define F first
 #define S second
 #define PI 3.1415926535897932384626
-#define deb(x) cout << #x << "=" << x << endl
-#define deb2(x, y) cout << #x << "=" << x << ", " << #y << "=" << y << endl
+#define sz(x) ((int)(x).size())
+
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
-typedef vector<int>	vi;
+typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef vector<ull> vull;
-typedef vector<pii>	vpii;
-typedef vector<pll>	vpll;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
 typedef vector<vi> vvi;
-typedef vector<vll>	vvll;
+typedef vector<vll> vvll;
 typedef vector<vull> vvull;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+
+/************************************************** DEBUGGER *******************************************************************************************************/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(vector <vector<T>> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) { cerr << "{"; _print(p.F); cerr << ","; _print(p.S); cerr << "}"; }
+template <class T> void _print(vector <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(vector <vector<T>> v) { cerr << "==>" << endl; for (vector<T> vec : v) { for(T i : vec) {_print(i); cerr << " "; } cerr << endl; } }
+template <class T> void _print(set <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(multiset <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(map <T, V> v) { cerr << "[ "; for (auto i : v) {_print(i); cerr << " "; } cerr << "]"; }
+
+/*******************************************************************************************************************************************************************/
+
 mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
 int rng(int lim) {
     uniform_int_distribution<int> uid(0,lim-1);
@@ -35,19 +73,28 @@ int rng(int lim) {
 const int INF = 0x3f3f3f3f;
 const int mod = 1e9+7;
 
-// dp[i] store the count of #distinct subseq. in prefix
+ll mod_exp(ll a, ll b) { a %= mod; if(a == 0) return 0LL; ll res = 1LL; 
+                         while(b > 0) { if(b & 1) res = (res * a) % mod; a = (a * a) % mod; b >>= 1; } return res; }
+                         
+ll mod_inv(ll a) { return mod_exp(a, mod - 2); } // works only for prime value of "mod"
+ll GCD(ll a, ll b) { return (b == 0) ? a : GCD(b, a % b); }
+
+/******************************************************************************************************************************/
+
+// dp[i] store the count of distinct subseq. in prefix
 // of length i (i.e. for s[0......i-1])
 vll dp;
 
-// map to store index of last occurence of a char 
+// map to store index of last occurrence of a char
 unordered_map<char, int> um;
 
 // Returns count of distinct sunsequences (including empty subseq) of 's'
 ll cnt_dis_seq(string &s) {
 	int n = s.length();
+	dp.clear();
 	dp.resize(n + 1);
 	
-	 // Empty string has only one subsequence
+	// Empty string has only one subsequence
 	dp[0] = 1;
 	
 	// Traverse through all lengths from 1 to n.
@@ -55,8 +102,8 @@ ll cnt_dis_seq(string &s) {
 		char ch = s[i-1];
 		dp[i] = 2 * dp[i-1];
 		
-		 // If current char has appeared before, then remove all 
-		 // subseq ending with previous occurrence.
+		// If current char has appeared before, then remove all 
+		// subseq ending with previous occurrence.
 		if(um.count(ch)) {
 			dp[i] -= dp[um[ch] - 1];
 		}
@@ -83,11 +130,17 @@ int main()
     //     freopen("input.txt", "r", stdin);
     //     freopen("output.txt", "w", stdout);
     // #endif
-
+    
+    // #ifndef ONLINE_JUDGE
+    //      freopen("error.txt", "w", stderr);
+    // #endif
+    
     int t = 1;
+    // int test = 1;
     // cin >> t;
     while(t--) {
-      solve();
+        // cout << "Case #" << test++ << ": ";
+        solve();
     }
 
     return 0;
