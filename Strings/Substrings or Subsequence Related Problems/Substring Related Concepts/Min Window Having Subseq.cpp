175f1957dc9b6c149fd9daa70026c0655b4a87cf
Kapil Choudhary <kapil181112309@gmail.com>
2021-07-16 13:15:25 +0530

    Files UPD

---
+++ Strings/Substrings or Subsequence Related Problems/Substring Related Concepts/Min Window Having Subseq.cpp
@@ -0,0 +1,155 @@
+// Prob: https://anshika-bhargava0202.medium.com/leetcode-727-minimum-window-subsequence-21c40baff689
+/*********************************************************************************************************/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define sz(x) ((int)(x).size())
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<vull> vvull;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef vector<vs> vvs;
+
+/************************************************** DEBUGGER *******************************************************************************************************/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(vector <vector<T>> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) { cerr << "{"; _print(p.F); cerr << ","; _print(p.S); cerr << "}"; }
+template <class T> void _print(vector <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(vector <vector<T>> v) { cerr << "==>" << endl; for (vector<T> vec : v) { for(T i : vec) {_print(i); cerr << " "; } cerr << endl; } }
+template <class T> void _print(set <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(multiset <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(map <T, V> v) { cerr << "[ "; for (auto i : v) {_print(i); cerr << " "; } cerr << "]"; }
+
+/*******************************************************************************************************************************************************************/
+
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+    uniform_int_distribution<int> uid(0,lim-1);
+    return uid(rang);
+}
+
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+
+ll mod_exp(ll a, ll b) { a %= mod; if(a == 0) return 0LL; ll res = 1LL; 
+                         while(b > 0) { if(b & 1) res = (res * a) % mod; a = (a * a) % mod; b >>= 1; } return res; }
+                         
+ll mod_inv(ll a) { return mod_exp(a, mod - 2); } // works only for prime value of "mod"
+ll GCD(ll a, ll b) { return (b == 0) ? a : GCD(b, a % b); }
+
+/******************************************************************************************************************************/
+
+// function which returns the min length substring s, such that t is a subsequence of that 
+// substring. If there are multiple such minimum-length windows, then this function return 
+// the one with the left-most starting index.
+
+string min_window(string &s, string &t) {
+    int n = sz(s), m = sz(t);
+    if(n < m) return "";
+    
+    // dp[i][j] = in substring s[0...i], if there exists a subsequence corresponding to 
+    //            t[0...j] then its starting index in string s = dp[i][j], else = -1.
+    vvi dp(n, vi(m, -1));
+    
+    dp[0][0] = (s[0] == t[0]) ? 0 : -1;
+    
+    for(int j = 1; j < m; j++) dp[0][j] = -1;
+    for(int i = 1; i < n; i++) {
+        if(s[i] == t[0]) dp[i][0] = i;
+        else if(i != 0) dp[i][0] = dp[i-1][0];
+    }
+    
+    for(int i = 1; i < n; i++) {
+        for(int j = 1; j < m; j++) {
+            if(s[i] == t[j]) dp[i][j] = dp[i-1][j-1];
+            else dp[i][j] = dp[i-1][j];
+        }
+    }
+    
+    int st = -1, len = INT_MAX;
+    
+    for(int i = 0; i < n; i++) {
+        int tmp = dp[i][m-1];
+        if(tmp != -1) {
+            int cur_len = i - tmp + 1;
+            if(cur_len < len) len = cur_len, st = tmp;
+        }
+    }
+    
+    if(len == INT_MAX) return "";
+    else return s.substr(st, len);
+}
+
+void solve()
+{
+  	string s, t; cin >> s >> t;
+  	
+  	string res = min_window(s, t);
+  	cout << sz(res) << "\n" << res << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    
+    // #ifndef ONLINE_JUDGE
+    //      freopen("error.txt", "w", stderr);
+    // #endif
+    
+    int t = 1;
+    // int test = 1;
+    // cin >> t;
+    while(t--) {
+        // cout << "Case #" << test++ << ": ";
+        solve();
+    }
+
+    return 0;
+}
