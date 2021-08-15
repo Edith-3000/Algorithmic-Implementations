86aa0a7750d3cda63f33205384e905dbb9ec26c0
Kapil Choudhary <kapil181112309@gmail.com>
2021-08-15 23:38:22 +0530

    Files UPD

---
+++ Dynamic Programming/DP on Grid/Problem - 1.cpp
@@ -0,0 +1,174 @@
+// Problem: B. Igor and his way to work
+// Contest: Codeforces - Training Contest #014
+// URL: https://codeforces.com/gym/340702/problem/B
+// Memory Limit: 256 MB
+// Time Limit: 3000 ms
+// Parsed on: 15-08-2021 08:53:20 IST (UTC+05:30)
+// Author: Kapil Choudhary
+// ********************************************************************
+// कर्मण्येवाधिकारस्ते मा फलेषु कदाचन |
+// मा कर्मफलहेतुर्भूर्मा ते सङ्गोऽस्त्वकर्मणि || १.४७ ||
+// ********************************************************************
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define sz(x) ((int)(x).size())
+#define vset(v, n, val) v.clear(); v.resize(n, val)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<vull> vvull;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef vector<vs> vvs;
+
+/************************************************** DEBUGGER *******************************************************************************************************/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(vector <vector<T>> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) { cerr << "{"; _print(p.F); cerr << ","; _print(p.S); cerr << "}"; }
+template <class T> void _print(vector <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(vector <vector<T>> v) { cerr << "==>" << endl; for (vector<T> vec : v) { for(T i : vec) {_print(i); cerr << " "; } cerr << endl; } }
+template <class T> void _print(set <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(multiset <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(map <T, V> v) { cerr << "[ "; for (auto i : v) {_print(i); cerr << " "; } cerr << "]"; }
+
+/*******************************************************************************************************************************************************************/
+
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+    uniform_int_distribution<int> uid(0,lim-1);
+    return uid(rang);
+}
+
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+
+ll mod_exp(ll a, ll b) { a %= mod; if(a == 0) return 0LL; ll res = 1LL; 
+                         while(b > 0) { if(b & 1) res = (res * a) % mod; a = (a * a) % mod; b >>= 1; } return res; }
+                         
+ll mod_inv(ll a) { return mod_exp(a, mod - 2); } // works only for prime value of "mod"
+ll GCD(ll a, ll b) { return (b == 0) ? a : GCD(b, a % b); }
+
+/******************************************************************************************************************************/
+
+vvc v;
+int dp[1000][1000][4];
+int n, m;
+bool possible;
+
+vi dx = {-1, 0, 1, 0};
+vi dy = {0, 1, 0, -1};
+
+void dfs(int i, int j, int dir, int turns) {
+	// base cases
+	if(i < 0 or i >= n or j < 0 or j >= m or turns > 2) return;
+	if(v[i][j] == '*') return;
+	
+	if(v[i][j] == 'T') {
+		possible = 1;
+		return;
+	}	
+	
+	// if there is already a better solution for this point and this direction 
+	if(dir != -1 and dp[i][j][dir] <= turns) return;
+	
+	if(dir != -1) dp[i][j][dir] = turns;
+		
+	for(int k = 0; k < 4; k++) {
+		int nx = i + dx[k], ny = j + dy[k];
+		if(dir == -1 or dir == k) dfs(nx, ny, k, turns);
+		else dfs(nx, ny, k, turns + 1);
+	}
+}
+
+// https://www.programmerall.com/article/8841838111/
+void solve()
+{
+  	cin >> n >> m;
+  	vset(v, n, vc(m));
+  	possible = 0;
+  	
+  	for(int i = 0; i < 1000; i++) {
+  		for(int j = 0; j < 1000; j++) {
+  			for(int k = 0; k < 4; k++) dp[i][j][k] = INF;
+  		}
+  	}
+  	
+  	int x, y;
+  	
+  	for(int i = 0; i < n; i++) {
+  		for(int j = 0; j < m; j++) {
+  			cin >> v[i][j];
+  			if(v[i][j] == 'S') x = i, y = j;
+  		}
+  	}
+  	  	
+  	dfs(x, y, -1, 0);
+  	
+  	if(possible) cout << "YES\n";  	
+  	else cout << "NO\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    
+    // #ifndef ONLINE_JUDGE
+    //      freopen("error.txt", "w", stderr);
+    // #endif
+    
+    int t = 1;
+    // int test = 1;
+    // cin >> t;
+    while(t--) {
+        // cout << "Case #" << test++ << ": ";
+        solve();
+    }
+
+    return 0;
+}
