a87ef23a009b4bd26f545543d0bbc1bf8cc52874
Kapil Choudhary <kapil181112309@gmail.com>
2021-09-15 00:06:02 +0530

    Files UPD

--- Advanced DS/Policy Based DS/Inversion Count using PBDS.cpp
+++ Advanced DS/Policy Based DS/Inversion Count using PBDS.cpp
@@ -19,30 +19,30 @@ using namespace __gnu_pbds;
 // pair<int, int> is taken so as to take care of array containing
 // duplicate elements
 typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag,
-		tree_order_statistics_node_update> PBDS;
+        tree_order_statistics_node_update> PBDS;
         
 int main()
 {
-	ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-	srand(chrono::high_resolution_clock::now().time_since_epoch().count());
-	
-	PBDS st;
-	
-	int inversion_cnt = 0; // for storing the result
-	int n; cin >> n;
-	for(int i = 0; i < n; i++){
-		int x; cin >> x;
-		inversion_cnt += (st.size() - st.order_of_key({x, i}));
-		st.insert({x, i});
-	}
-	
-	cout << inversion_cnt;
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    
+    PBDS st;
+    
+    int inversion_cnt = 0; // for storing the result
+    int n; cin >> n;
+    for(int i = 0; i < n; i++){
+        int x; cin >> x;
+        inversion_cnt += (st.size() - st.order_of_key({x, i}));
+        st.insert({x, i});
+    }
+    
+    cout << inversion_cnt;
 }
 
 // Time complexity: O(n x log(n))
 
 /* Sample i/p: 5
-			   1 20 6 4 5
+               1 20 6 4 5
    
    Sample o/p: 5
 */
