bbacff2ddb02fecf7986b506ed8339d183cbb945
Edith-3000 <kapil181112309@gmail.com>
2022-07-17 10:55:21 +0530

    Files UPD

--- Dynamic Programming/Pattern - MCM/Scramble String.cpp
+++ Dynamic Programming/Pattern - MCM/Scramble String.cpp
@@ -359,6 +359,7 @@ void solve()
     string s1, s2; cin >> s1 >> s2;
     
     dp.clear();
+
     if(is_scramble(s1, s2)) cout << "YES\n";
     else cout << "NO\n";
 }
@@ -389,6 +390,194 @@ int main()
 }
 
 // Time complexity: ??
+
+/*****************************************************************************************************/
+
+// OPTIMIZATION OF ABOVE METHOD
+
+/* The above method was not getting an AC on LeetCode: https://leetcode.com/problems/scramble-string/
+// So tried with using unordered_map instead of map......but in that also it gave TLE with the message:--->
+   "288 / 288 test cases passed, but took too long."
+
+   Therefore to further avoid recursive call a check is performed in this method which find out if the 
+   frequency of characters is same in both the strings or not....and it got passed on LeetCode.    
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define sz(x) ((int)(x).size())
+#define vset(v, n, val) v.clear(); v.resize(n, val)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<vull> vvull;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef vector<vs> vvs;
+
+/************************************************** DEBUGGER *******************************************************************************************************/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(vector <vector<T>> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(multimap <T, V> v);
+template <class T> void _print(queue <T> v);
+template <class T> void _print(priority_queue <T> v);
+template <class T> void _print(stack <T> s);
+
+// modify it's definition below as per need such as it can be used for STL containers with custom args passed
+template <class T> void _print(T v); 
+
+template <class T, class V> void _print(pair <T, V> p) { cerr << "{"; _print(p.F); cerr << ","; _print(p.S); cerr << "}"; }
+template <class T> void _print(vector <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(vector <vector<T>> v) { cerr << "==>" << endl; for (vector<T> vec : v) { for(T i : vec) {_print(i); cerr << " "; } cerr << endl; } }
+template <class T> void _print(set <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(map <T, V> v) { cerr << "[ "; for (auto i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(multiset <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(multimap <T, V> v) { cerr << "[ "; for (auto i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(queue <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.front()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(priority_queue <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.top()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(stack <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.top()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(T v) {  }
+
+/*******************************************************************************************************************************************************************/
+
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+
+ll mod_exp(ll a, ll b) { a %= mod; if(a == 0) return 0LL; ll res = 1LL; 
+                         while(b > 0) { if(b & 1) res = (res * a) % mod; a = (a * a) % mod; b >>= 1; } return res; }
+                         
+ll mod_inv(ll a) { return mod_exp(a, mod - 2); } // works only for prime value of "mod"
+ll GCD(ll a, ll b) { return (b == 0) ? a : GCD(b, a % b); }
+
+/******************************************************************************************************************************/
+
+unordered_map<string, bool> dp;
+
+bool is_scramble(string &s1, string &s2) {
+    int n = sz(s1), m = sz(s2);
+    
+    // base case(s)
+    if(s1 == s2) return 1;
+    if(n != m) return 0;
+    if(n <= 1) return 0;
+    
+    // forming a unique key(type: string) of the form: "s1 s2"
+    string key = s1;
+    key.pb(' ');
+    key.append(s2);
+    
+    // check if already calculated or not
+    if(dp.find(key) != dp.end()) return dp[key]; 
+    
+    // Pruning: Avoid Unnecessary recursion calls
+    vector<int> cnt1(26, 0), cnt2(26, 0);
+    
+    for(int i = 0; i < n; i++) {
+        cnt1[s1[i] - 'a'] += 1;
+        cnt2[s2[i] - 'a'] += 1;
+    }
+    
+    if(cnt1 != cnt2) {
+        return dp[key] = 0;
+    }
+    
+    // choice diagram code
+    // to return final result, initially assuming that strings are not scrambled
+    bool res = 0;
+    
+    for(int i = 1; i <= (n - 1); i++) {
+        string ss1 = s1.substr(0, i), ss2 = s2.substr(n-i, i);
+        string ss3 = s1.substr(i, n-i), ss4 = s2.substr(0, n-i);
+        
+        // possibility 1: if swapping was done
+        bool op1 = is_scramble(ss1, ss2) and is_scramble(ss3, ss4);
+        
+        ss1 = s1.substr(0, i), ss2 = s2.substr(0, i);
+        ss3 = s1.substr(i, n-i), ss4 = s2.substr(i, n-i);
+        
+        // possibility 2: if swapping was not done
+        bool op2 = is_scramble(ss1, ss2) and is_scramble(ss3, ss4);
+                   
+        if(op1 or op2) { res = 1; break; }
+    }
+    
+    return dp[key] = res;
+}
+
+void solve()
+{
+    string s1, s2; cin >> s1 >> s2;
+    
+    dp.clear();
+    
+    if(is_scramble(s1, s2)) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    
+    // #ifndef ONLINE_JUDGE
+    //      freopen("error.txt", "w", stderr);
+    // #endif
+    
+    int t = 1;
+    // int test = 1;
+    // cin >> t;
+    while(t--) {
+        // cout << "Case #" << test++ << ": ";
+        solve();
+    }
+
+    return 0;
+}
 
 /*****************************************************************************************************/
 
