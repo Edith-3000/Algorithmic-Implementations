412eb7b493b5ed4488b086197f2c9455a4295274
Edith-3000 <kapil181112309@gmail.com>
2022-09-06 10:13:05 +0530

    Files UPD

--- Graph/Tree/BST/Serialize and Deserialize BST.cpp
+++ Graph/Tree/BST/Serialize and Deserialize BST.cpp
@@ -1,6 +1,6 @@
-// Prob: https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
+// Prob: https://leetcode.com/problems/serialize-and-deserialize-bst/
 
-// Ref: https://www.youtube.com/watch?v=-YbXySKJsX8
+// Ref: Similar to "Serialize and Deserialize BT.cpp"
 
 // Other useful links: https://www.geeksforgeeks.org/stringstream-c-applications/
 //                     https://www.geeksforgeeks.org/getline-string-c/
@@ -14,6 +14,8 @@ using namespace std;
 #define ull unsigned long long
 #define pb push_back
 #define ppb pop_back
+#define pf push_front
+#define ppf pop_front
 #define mp make_pair
 #define F first
 #define S second
@@ -60,20 +62,27 @@ template <class T> void _print(vector <vector<T>> v);
 template <class T> void _print(set <T> v);
 template <class T, class V> void _print(map <T, V> v);
 template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(multimap <T, V> v);
+template <class T> void _print(queue <T> v);
+template <class T> void _print(priority_queue <T> v);
+template <class T> void _print(stack <T> s);
+
+// modify it's definition below as per need such as it can be used for STL containers with custom args passed
+template <class T> void _print(T v); 
+
 template <class T, class V> void _print(pair <T, V> p) { cerr << "{"; _print(p.F); cerr << ","; _print(p.S); cerr << "}"; }
 template <class T> void _print(vector <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
 template <class T> void _print(vector <vector<T>> v) { cerr << "==>" << endl; for (vector<T> vec : v) { for(T i : vec) {_print(i); cerr << " "; } cerr << endl; } }
 template <class T> void _print(set <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
-template <class T> void _print(multiset <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
 template <class T, class V> void _print(map <T, V> v) { cerr << "[ "; for (auto i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(multiset <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(multimap <T, V> v) { cerr << "[ "; for (auto i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(queue <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.front()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(priority_queue <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.top()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(stack <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.top()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(T v) {  }
 
 /*******************************************************************************************************************************************************************/
-
-mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
-int rng(int lim) {
-    uniform_int_distribution<int> uid(0,lim-1);
-    return uid(rang);
-}
 
 const int INF = 0x3f3f3f3f;
 const int mod = 1e9+7;
@@ -165,15 +174,13 @@ class Codec {
 
 void solve()
 {
-  	TreeNode* root = new TreeNode(1);
+  	TreeNode* root = new TreeNode(4);
 	root->left = new TreeNode(2);
-	root->right = new TreeNode(3);
-	root->right->left = new TreeNode(4);
-	root->right->right = new TreeNode(5);
-	root->right->left->left = new TreeNode(6);
-	root->right->left->right = new TreeNode(7);
-	root->right->left->right->left = new TreeNode(8);
-	root->right->left->right->right = new TreeNode(9);
+	root->right = new TreeNode(6);
+	root->left->left = new TreeNode(1);
+	root->left->right = new TreeNode(3);
+	root->right->left = new TreeNode(5);
+	root->right->right = new TreeNode(7);
 	
 	class Codec ser, deser;
 	
@@ -183,7 +190,6 @@ void solve()
 int main()
 {
     ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
 
     // #ifndef ONLINE_JUDGE
     //     freopen("input.txt", "r", stdin);
