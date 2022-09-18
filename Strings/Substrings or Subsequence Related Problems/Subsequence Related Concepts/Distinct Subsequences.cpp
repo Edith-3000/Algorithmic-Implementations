9cb5e4fa8ae099a72607b7e175227847b777c69d
Edith-3000 <kapil181112309@gmail.com>
2022-09-18 16:06:15 +0530

    Files UPD

--- Strings/Substrings or Subsequence Related Problems/Subsequence Related Concepts/Distinct Subsequences.cpp
+++ Strings/Substrings or Subsequence Related Problems/Subsequence Related Concepts/Distinct Subsequences.cpp
@@ -1,9 +1,14 @@
 // Problem: https://www.interviewbit.com/problems/distinct-subsequences/
+//          https://leetcode.com/problems/distinct-subsequences/
+
 // Ref: https://www.geeksforgeeks.org/count-distinct-occurrences-as-a-subsequence/
 //      https://medium.com/@vivek_ranjan/distinct-subsequences-adde0692e19c
-/******************************************************************************************************/
+//      https://www.youtube.com/watch?v=nVG7eTiD2bY&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=34&ab_channel=takeUforward
+/*****************************************************************************************************************************/
 
-// RECURSIVE APPROACH
+// METHOD - 1 (RECURSIVE APPROACH)
+
+// LEGACY CONTENT: https://pastebin.com/AUPiH29P
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -82,27 +87,25 @@ ll GCD(ll a, ll b) { return (b == 0) ? a : GCD(b, a % b); }
 
 /******************************************************************************************************************************/
 
-int cnt_ways(string &s1, string &s2, int n, int m) {
-	// base case(s)
-	if(n == 0 and m != 0) return 0;
-	if(n < m) return 0;
-	if(n == 0 or m == 0) return 1;
-	
-	if(s1[n-1] == s2[m-1]) {
-		return cnt_ways(s1, s2, n - 1, m) + cnt_ways(s1, s2, n - 1, m - 1);
-	}
-	
-	else return cnt_ways(s1, s2, n - 1, m);
+// Function to count the #distinct subsequences of string s which equals string t.
+int cnt_ways(string &s, string &t, int n, int m) {
+    // base case(s)
+    if(m == 0) return 1;
+    if(n == 0) return 0;
+
+    // choice diagram code
+    if(s[n - 1] != t[m - 1]) return calc(s, t, n - 1, m);
+    else return calc(s, t, n - 1, m) + calc(s, t, n - 1, m - 1);
 }
 
 void solve()
 {
-  	string s1, s2; cin >> s1 >> s2;
-  	
-  	int n = sz(s1);
-  	int m = sz(s2);
-  	
-  	cout << cnt_ways(s1, s2, n, m) << "\n";
+  	string s, t; cin >> s >> t;
+    
+    int n = sz(s);
+    int m = sz(t);
+    
+    cout << cnt_ways(s, t, n, m) << "\n";
 }
 
 int main()
@@ -134,7 +137,9 @@ int main()
 
 /*****************************************************************************************************/
 
-// MEMOIZED IMPLEMENTATION
+// METHOD - 2 (MEMOIZED IMPLEMENTATION)
+
+// LEGACY CONTENT: https://pastebin.com/dtPNM8St
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -215,33 +220,31 @@ ll GCD(ll a, ll b) { return (b == 0) ? a : GCD(b, a % b); }
 
 vvi dp;
 
-int cnt_ways(string &s1, string &s2, int n, int m) {
-	// base case(s)
-	if(n == 0 and m != 0) return 0;
-	if(n < m) return 0;
-	if(n == 0 or m == 0) return 1;
-	
-	// check if already calculated or not
-	if(dp[n][m] != -1) return dp[n][m];
-	
-	if(s1[n-1] == s2[m-1]) {
-		return dp[n][m] = cnt_ways(s1, s2, n - 1, m) + cnt_ways(s1, s2, n - 1, m - 1);
-	}
-	
-	else return dp[n][m] = cnt_ways(s1, s2, n - 1, m);
+// Function to count the #distinct subsequences of string s which equals string t.
+int cnt_ways(string &s, string &t, int n, int m) {
+    // base case(s)
+    if(m == 0) return 1;
+    if(n == 0) return 0;
+
+    // check if already calculated or not
+    if(dp[n][m] != -1) return dp[n][m];
+
+    // choice diagram code
+    if(s[n - 1] != t[m - 1]) return dp[n][m] = calc(s, t, n - 1, m);
+    else return dp[n][m] = calc(s, t, n - 1, m) + calc(s, t, n - 1, m - 1);
 }
 
 void solve()
 {
-  	string s1, s2; cin >> s1 >> s2;
+  	string s, t; cin >> s >> t;
   	
-  	int n = sz(s1);
-  	int m = sz(s2);
+  	int n = sz(s);
+  	int m = sz(t);
   	
   	dp.clear();
   	dp.resize(n + 1, vi(m + 1, -1));
   	
-  	cout << cnt_ways(s1, s2, n, m) << "\n";
+  	cout << cnt_ways(s, t, n, m) << "\n";
 }
 
 int main()
@@ -274,7 +277,7 @@ int main()
 
 /*****************************************************************************************************/
 
-// TABULATION IMPLEMENTATION
+// METHOD - 3 (TABULATION IMPLEMENTATION)
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -353,17 +356,19 @@ ll GCD(ll a, ll b) { return (b == 0) ? a : GCD(b, a % b); }
 
 /******************************************************************************************************************************/
 
-vvi dp;
+// Function to count the #distinct subsequences of string s which equals string t.
+int cnt_ways(string &s, string &t, int n, int m) {
+    vvi dp(n + 1, vi(m + 1));
 
-int cnt_ways(string &s1, string &s2, int n, int m) {
 	// initialisation of dp matrix
 	for(int i = 0; i <= n; i++) dp[i][0] = 1;
 	for(int j = 1; j <= m; j++) dp[0][j] = 0;
 	
+    // choice diagram code iterative version
 	for(int i = 1; i <= n; i++) {
 		for(int j = 1; j <= m; j++) {
-			if(s1[i-1] == s2[j-1]) dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
-			else dp[i][j] = dp[i-1][j];
+			if(s[i - 1] == t[j - 1]) dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+			else dp[i][j] = dp[i - 1][j];
 		}
 	}
 	
@@ -372,15 +377,12 @@ int cnt_ways(string &s1, string &s2, int n, int m) {
 
 void solve()
 {
-  	string s1, s2; cin >> s1 >> s2;
-  	
-  	int n = sz(s1);
-  	int m = sz(s2);
-  	
-  	dp.clear();
-  	dp.resize(n + 1, vi(m + 1, -1));
-  	
-  	cout << cnt_ways(s1, s2, n, m) << "\n";
+  	string s, t; cin >> s >> t;
+    
+    int n = sz(s);
+    int m = sz(t);
+    
+    cout << cnt_ways(s, t, n, m) << "\n";
 }
 
 int main()
@@ -413,10 +415,13 @@ int main()
 
 /*****************************************************************************************************/
 
-// SPACE OPTIMIZED TABULATION IMPLEMENTATION
-// We can observe from above tabulation approache that in the inner loop, we only consider values of 
+// METHOD - 4
+
+// SPACE OPTIMIZED TABULATION IMPLEMENTATION (BY MAKING USE OF 2 VECTORS)
+
+// We can observe from above tabulation approach that in the inner loop, we only consider values of 
 // the previous iteration, i.e. the values in the previous row of the dp matrix. 
-// Therefore, to optimise the space complexity of algorithm, we could use by keeping only two arrays, 
+// Therefore, to optimise the space complexity of algorithm, we could keep only two arrays, 
 // instead of the entire matrix.
 
 #include<bits/stdc++.h>
@@ -496,37 +501,190 @@ ll GCD(ll a, ll b) { return (b == 0) ? a : GCD(b, a % b); }
 
 /******************************************************************************************************************************/
 
-int cnt_ways(string &s1, string &s2, int n, int m) {
+// Function to count the #distinct subsequences of string s which equals string t.
+int cnt_ways(string &s, string &t, int n, int m) {
 	vi prev_row(m + 1), curr_row(m + 1);
+
 	prev_row[0] = 1; curr_row[0] = 1;
+
 	for(int j = 1; j <= m; j++) prev_row[j] = 0;
 		
 	for(int i = 1; i <= n; i++) { 
 		for(int j = 1; j <= m; j++) {
-			if(s1[i-1] == s2[j-1]) curr_row[j] = prev_row[j] + prev_row[j-1];
+			if(s[i - 1] == t[j - 1]) curr_row[j] = prev_row[j] + prev_row[j - 1];
 			else curr_row[j] = prev_row[j];
 		}
 		
 		prev_row = curr_row;
 	}
 	
-	return curr_row[m];
+	return prev_row[m];
 }
 
 void solve()
 {
-  	string s1, s2; cin >> s1 >> s2;
+  	string s, t; cin >> s >> t;
   	
-  	int n = sz(s1);
-  	int m = sz(s2);
+  	int n = sz(s);
+  	int m = sz(t);
   	
-  	cout << cnt_ways(s1, s2, n, m) << "\n";
+  	cout << cnt_ways(s, t, n, m) << "\n";
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    
+    // #ifndef ONLINE_JUDGE
+    //      freopen("error.txt", "w", stderr);
+    // #endif
+    
+    int t = 1;
+    // int test = 1;
+    // cin >> t;
+    while(t--) {
+        // cout << "Case #" << test++ << ": ";
+        solve();
+    }
+
+    return 0;
+}
+
+// Time complexity: O(n x m)
+// Space complexity: O(m)
+
+/***********************************************************************************************************************************/
+
+// METHOD - 5
+
+// SPACE OPTIMIZED TABULATION IMPLEMENTATION (BY MAKING USE OF 1 VECTOR)
+// NOTE: Although asymptotic space complexity is same as above "METHOD - 4".
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define sz(x) ((int)(x).size())
+#define vset(v, n, val) v.clear(); v.resize(n, val)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<vull> vvull;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef vector<vs> vvs;
+
+/************************************************** DEBUGGER *******************************************************************************************************/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(vector <vector<T>> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(multimap <T, V> v);
+template <class T> void _print(queue <T> v);
+template <class T> void _print(priority_queue <T> v);
+template <class T> void _print(stack <T> s);
+
+// modify it's definition below as per need such as it can be used for STL containers with custom args passed
+template <class T> void _print(T v); 
+
+template <class T, class V> void _print(pair <T, V> p) { cerr << "{"; _print(p.F); cerr << ","; _print(p.S); cerr << "}"; }
+template <class T> void _print(vector <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(vector <vector<T>> v) { cerr << "==>" << endl; for (vector<T> vec : v) { for(T i : vec) {_print(i); cerr << " "; } cerr << endl; } }
+template <class T> void _print(set <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(map <T, V> v) { cerr << "[ "; for (auto i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(multiset <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(multimap <T, V> v) { cerr << "[ "; for (auto i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(queue <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.front()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(priority_queue <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.top()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(stack <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.top()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(T v) {  }
+
+/*******************************************************************************************************************************************************************/
+
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+
+ll mod_exp(ll a, ll b) { a %= mod; if(a == 0) return 0LL; ll res = 1LL; 
+                         while(b > 0) { if(b & 1) res = (res * a) % mod; a = (a * a) % mod; b >>= 1; } return res; }
+                         
+ll mod_inv(ll a) { return mod_exp(a, mod - 2); } // works only for prime value of "mod"
+ll GCD(ll a, ll b) { return (b == 0) ? a : GCD(b, a % b); }
+
+/******************************************************************************************************************************/
+
+// Function to count the #distinct subsequences of string s which equals string t.
+int cnt_ways(string &s, string &t, int n, int m) {
+    vi dp(m + 1);
+
+    dp[0] = 1;
+
+    for(int j = 1; j <= m; j++) dp[j] = 0;
+        
+    for(int i = 1; i <= n; i++) { 
+        for(int j = m; j >= 1; j--) {
+            if(s[i - 1] == t[j - 1]) dp[j] = dp[j] + dp[j - 1];
+        }
+    }
+    
+    return dp[m];
+}
+
+void solve()
+{
+    string s, t; cin >> s >> t;
+    
+    int n = sz(s);
+    int m = sz(t);
+    
+    cout << cnt_ways(s, t, n, m) << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 
     // #ifndef ONLINE_JUDGE
     //     freopen("input.txt", "r", stdin);
---
+++ Strings/Substrings or Subsequence Related Problems/Subsequence Related Concepts/Sequence Pattern Matching.cpp
@@ -0,0 +1,312 @@
+/* PROBLEM: Given 2 strings s and t. Find out whether s is a subsequence of t or not? 
+            Return a boolean value (True/False).
+
+   Link: https://leetcode.com/problems/is-subsequence/
+*/
+
+/***********************************************************************************************************************************/
+
+// METHOD - 1 (LCS BASED)
+// Ref: https://www.youtube.com/watch?v=QVntmksK2es&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=31&ab_channel=AdityaVerma
+
+/* Underlying concept :---> 
+   
+   # s is a subsequence of t iff ===> LCS(s, t) == s.
+
+   # But here there is no need to find the complete LCS(s, t), only LCS_Length(s, t) will suffice, as:
+     ⇒ LCS_Length(s, t) ϵ [0, min(n, m)],
+       where n = s.length(), m = t.length()
+*/
+
+/***********************************************************************************************************************************/
+
+// METHOD - 2 (2 POINTER BASED)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define sz(x) ((int)(x).size())
+#define vset(v, n, val) v.clear(); v.resize(n, val)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<vull> vvull;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef vector<vs> vvs;
+
+/************************************************** DEBUGGER *******************************************************************************************************/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(vector <vector<T>> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(multimap <T, V> v);
+template <class T> void _print(queue <T> v);
+template <class T> void _print(priority_queue <T> v);
+template <class T> void _print(stack <T> s);
+
+// modify it's definition below as per need such as it can be used for STL containers with custom args passed
+template <class T> void _print(T v); 
+
+template <class T, class V> void _print(pair <T, V> p) { cerr << "{"; _print(p.F); cerr << ","; _print(p.S); cerr << "}"; }
+template <class T> void _print(vector <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(vector <vector<T>> v) { cerr << "==>" << endl; for (vector<T> vec : v) { for(T i : vec) {_print(i); cerr << " "; } cerr << endl; } }
+template <class T> void _print(set <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(map <T, V> v) { cerr << "[ "; for (auto i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(multiset <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(multimap <T, V> v) { cerr << "[ "; for (auto i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(queue <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.front()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(priority_queue <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.top()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(stack <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.top()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(T v) {  }
+
+/*******************************************************************************************************************************************************************/
+
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+
+ll mod_exp(ll a, ll b) { a %= mod; if(a == 0) return 0LL; ll res = 1LL; 
+                         while(b > 0) { if(b & 1) res = (res * a) % mod; a = (a * a) % mod; b >>= 1; } return res; }
+                         
+ll mod_inv(ll a) { return mod_exp(a, mod - 2); } // works only for prime value of "mod"
+ll GCD(ll a, ll b) { return (b == 0) ? a : GCD(b, a % b); }
+
+/******************************************************************************************************************************/
+
+// Function to check if 's' occurs as a subsequence in 't'
+bool is_subsequence(string &s, string &t) {
+	int n = sz(s), m = sz(t);
+	
+	int idx = 0;
+	
+	for(int i = 0; i < m; i++) {
+		if(idx == n) return 1;
+		if(s[idx] == t[i]) idx += 1;
+	}
+	
+	return (idx == n);
+}
+
+void solve()
+{
+  	string s, t; cin >> s >> t;
+  	
+  	if(is_subsequence(s, t)) cout << "YES\n";
+  	else cout << "NO\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    
+    // #ifndef ONLINE_JUDGE
+    //      freopen("error.txt", "w", stderr);
+    // #endif
+    
+    int t = 1;
+    // int test = 1;
+    // cin >> t;
+    while(t--) {
+        // cout << "Case #" << test++ << ": ";
+        solve();
+    }
+
+    return 0;
+}
+
+/***********************************************************************************************************************************************/
+
+// METHOD - 3 (HASHING BASED)
+
+// METHOD - 2 will be unsuitable if there are many queries to process (like in "https://leetcode.com/problems/number-of-matching-subsequences/", 
+// hence this method can be used.
+
+// See "Follow up" of LeetCode problem link.
+
+// Ref: https://youtu.be/PwQnydsKk_I?t=257
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define sz(x) ((int)(x).size())
+#define vset(v, n, val) v.clear(); v.resize(n, val)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<vull> vvull;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef vector<vs> vvs;
+
+/************************************************** DEBUGGER *******************************************************************************************************/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(vector <vector<T>> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(multimap <T, V> v);
+template <class T> void _print(queue <T> v);
+template <class T> void _print(priority_queue <T> v);
+template <class T> void _print(stack <T> s);
+
+// modify it's definition below as per need such as it can be used for STL containers with custom args passed
+template <class T> void _print(T v); 
+
+template <class T, class V> void _print(pair <T, V> p) { cerr << "{"; _print(p.F); cerr << ","; _print(p.S); cerr << "}"; }
+template <class T> void _print(vector <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(vector <vector<T>> v) { cerr << "==>" << endl; for (vector<T> vec : v) { for(T i : vec) {_print(i); cerr << " "; } cerr << endl; } }
+template <class T> void _print(set <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(map <T, V> v) { cerr << "[ "; for (auto i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(multiset <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(multimap <T, V> v) { cerr << "[ "; for (auto i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(queue <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.front()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(priority_queue <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.top()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(stack <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.top()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(T v) {  }
+
+/*******************************************************************************************************************************************************************/
+
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+
+ll mod_exp(ll a, ll b) { a %= mod; if(a == 0) return 0LL; ll res = 1LL; 
+                         while(b > 0) { if(b & 1) res = (res * a) % mod; a = (a * a) % mod; b >>= 1; } return res; }
+                         
+ll mod_inv(ll a) { return mod_exp(a, mod - 2); } // works only for prime value of "mod"
+ll GCD(ll a, ll b) { return (b == 0) ? a : GCD(b, a % b); }
+
+/******************************************************************************************************************************/
+
+// Function to check if 's' occurs as a subsequence in 't'
+bool is_subsequence(string &s, string &t) {
+	int n = sz(s), m = sz(t);
+	
+	// assuming that both strings consists of lowercase latin letters
+	vvi positions(26);
+	
+	for(int j = 0; j < m; j++) {
+		positions[t[j] - 'a'].pb(j);
+	}
+	
+	int prev = -1;
+	
+	for(int i = 0; i < n; i++) {
+		auto it = upper_bound(positions[s[i] - 'a'].begin(), positions[s[i] - 'a'].end(), prev);
+		if(it == positions[s[i] - 'a'].end()) return 0;
+		prev = *it;
+	}
+	
+	return 1;
+}
+
+void solve()
+{
+  	string s, t; cin >> s >> t;
+  	
+  	if(is_subsequence(s, t)) cout << "YES\n";
+  	else cout << "NO\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    
+    // #ifndef ONLINE_JUDGE
+    //      freopen("error.txt", "w", stderr);
+    // #endif
+    
+    int t = 1;
+    // int test = 1;
+    // cin >> t;
+    while(t--) {
+        // cout << "Case #" << test++ << ": ";
+        solve();
+    }
+
+    return 0;
+}
