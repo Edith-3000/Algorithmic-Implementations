a4bceaa4ec68a7872fd2c51cba83e8b2169966cc
Edith-3000 <kapil181112309@gmail.com>
2022-09-20 09:54:35 +0530

    Files UPD

---
+++ Dynamic Programming/DP on Grid/Subrectangle or Subsquare Concepts/Count Square Submatrices with All Ones.cpp
@@ -0,0 +1,175 @@
+// Prob: https://leetcode.com/problems/count-square-submatrices-with-all-ones/
+
+// Ref: https://www.youtube.com/watch?v=auS1fynpnjo&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=57&ab_channel=takeUforward
+
+/*************************************************************************************************************************************/
+
+// METHOD - 1
+
+/* # Generate all the submatrices and if it's a square submatrix and contains all ones add 1 to the count.
+     Refer "Matrix/Generate All Submatrices of a Matrix.cpp" file to generate all the submatrices.
+*/
+
+/*************************************************************************************************************************************/
+
+// METHOD - 2 (DP BASED)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define sz(x) ((int)(x).size())
+#define vset(v, n, val) v.clear(); v.resize(n, val)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<vull> vvull;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef vector<vs> vvs;
+
+/************************************************** DEBUGGER *******************************************************************************************************/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(vector <vector<T>> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(multimap <T, V> v);
+template <class T> void _print(queue <T> v);
+template <class T> void _print(priority_queue <T> v);
+template <class T> void _print(stack <T> s);
+
+// modify it's definition below as per need such as it can be used for STL containers with custom args passed
+template <class T> void _print(T v); 
+
+template <class T, class V> void _print(pair <T, V> p) { cerr << "{"; _print(p.F); cerr << ","; _print(p.S); cerr << "}"; }
+template <class T> void _print(vector <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(vector <vector<T>> v) { cerr << "==>" << endl; for (vector<T> vec : v) { for(T i : vec) {_print(i); cerr << " "; } cerr << endl; } }
+template <class T> void _print(set <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(map <T, V> v) { cerr << "[ "; for (auto i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(multiset <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(multimap <T, V> v) { cerr << "[ "; for (auto i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(queue <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.front()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(priority_queue <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.top()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(stack <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.top()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(T v) {  }
+
+/*******************************************************************************************************************************************************************/
+
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+
+ll mod_exp(ll a, ll b) { a %= mod; if(a == 0) return 0LL; ll res = 1LL; 
+                         while(b > 0) { if(b & 1) res = (res * a) % mod; a = (a * a) % mod; b >>= 1; } return res; }
+                         
+ll mod_inv(ll a) { return mod_exp(a, mod - 2); } // works only for prime value of "mod"
+ll GCD(ll a, ll b) { return (b == 0) ? a : GCD(b, a % b); }
+
+/******************************************************************************************************************************/
+
+int count_squares(vvi &v) {
+	int n = sz(v);
+	if(n == 0) return 0;
+	
+	int m = sz(v[0]);
+	
+	int res = 0;
+	
+	// dp[i][j] = #square submatrices with all ones such that their bottom right cell is (i, j)
+	vvi dp(n, vi(m));
+	
+	for(int i = 0; i < n; i++) {
+		dp[i][0] = v[i][0];
+		res += dp[i][0];
+	}
+	
+	for(int j = 1; j < m; j++) {
+		dp[0][j] = v[0][j];
+		res += dp[0][j];
+	}
+		
+	for(int i = 1; i < n; i++) {
+		for(int j = 1; j < m; j++) {
+			if(v[i][j] == 0) dp[i][j] = 0;
+			else dp[i][j] = min({dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]}) + v[i][j];
+			res += dp[i][j];
+		}
+	}
+	
+	return res;
+}
+
+void solve()
+{
+  	int n, m; cin >> n >> m;
+  	
+  	vvi v(n, vi(m));
+  	
+  	for(int i = 0; i < n; i++) {
+  		for(int j = 0; j < m; j++) {
+  			cin >> v[i][j];
+  		}
+  	}
+  	
+  	cout << count_squares(v) << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    
+    // #ifndef ONLINE_JUDGE
+    //      freopen("error.txt", "w", stderr);
+    // #endif
+    
+    int t = 1;
+    // int test = 1;
+    // cin >> t;
+    while(t--) {
+        // cout << "Case #" << test++ << ": ";
+        solve();
+    }
+
+    return 0;
+}
