72dd3f7b1a51af7cb85f213dcf5da43e9e0d60c7
Edith-3000 <kapil181112309@gmail.com>
2022-10-12 23:00:26 +0530

    Files UPD

---
+++ Dynamic Programming/DP on Grid/Paths in Matrix Whose Sum Is Divisible by K.cpp
@@ -0,0 +1,167 @@
+// Prob: https://leetcode.com/problems/paths-in-matrix-whose-sum-is-divisible-by-k/
+
+// Ref: https://www.youtube.com/watch?v=zWoUoQ5svro
+
+/*********************************************************************************************************************************************************/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.1415926535897932384626
+#define sz(x) ((int)(x).size())
+#define vset(v, n, val) v.clear(); v.resize(n, val)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<bool> vb;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<vull> vvull;
+typedef vector<vb> vvb;
+typedef vector<vc> vvc;
+typedef vector<vs> vvs;
+
+/************************************************** DEBUGGER *******************************************************************************************************/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(vector <vector<T>> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(multimap <T, V> v);
+template <class T> void _print(queue <T> v);
+template <class T> void _print(priority_queue <T> v);
+template <class T> void _print(stack <T> s);
+
+// modify it's definition below as per need such as it can be used for STL containers with custom args passed
+template <class T> void _print(T v); 
+
+template <class T, class V> void _print(pair <T, V> p) { cerr << "{"; _print(p.F); cerr << ","; _print(p.S); cerr << "}"; }
+template <class T> void _print(vector <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(vector <vector<T>> v) { cerr << "==>" << endl; for (vector<T> vec : v) { for(T i : vec) {_print(i); cerr << " "; } cerr << endl; } }
+template <class T> void _print(set <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(map <T, V> v) { cerr << "[ "; for (auto i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(multiset <T> v) { cerr << "[ "; for (T i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T, class V> void _print(multimap <T, V> v) { cerr << "[ "; for (auto i : v) {_print(i); cerr << " "; } cerr << "]"; }
+template <class T> void _print(queue <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.front()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(priority_queue <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.top()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(stack <T> v) { cerr << "[ "; while(!v.empty()) {_print(v.top()); v.pop(); cerr << " "; } cerr << "]"; }
+template <class T> void _print(T v) {  }
+
+/*******************************************************************************************************************************************************************/
+
+const int INF = 0x3f3f3f3f;
+const ll mod = 1e9+7;
+
+ll mod_exp(ll a, ll b) { a %= mod; if(a == 0) return 0LL; ll res = 1LL; 
+                         while(b > 0) { if(b & 1) res = (res * a) % mod; a = (a * a) % mod; b >>= 1; } return res; }
+                         
+ll mod_inv(ll a) { return mod_exp(a, mod - 2); } // works only for prime value of "mod"
+ll GCD(ll a, ll b) { return (b == 0) ? a : GCD(b, a % b); }
+
+/******************************************************************************************************************************/
+
+// dp[i][j][l] = #paths from (i, j) to (n - 1, m - 1) such that the sum of elements in the path % k = l
+vector<vector<vector<ll>>> dp;
+
+ll cnt_paths(ll i, ll j, ll mod_val, ll k, ll n, ll m, vvll &v) {
+	// base case(s)
+	if((i == n - 1) and (j == m - 1)) {
+		return ((v[i][j] % k) == mod_val);
+	}
+	
+	// check if already calculated
+	if(dp[i][j][mod_val] != -1) {
+            return dp[i][j][mod_val];
+        }
+	
+	ll ways_down = 0LL, ways_right = 0LL;
+	
+	ll new_mod_val = (mod_val - (v[i][j] % k) + k) % k;
+	
+	if((i + 1) < n) {
+		ways_down = cnt_paths(i + 1, j, new_mod_val, k, n, m, v);
+	}
+	
+	if((j + 1) < m) {
+		ways_right = cnt_paths(i, j + 1, new_mod_val, k, n, m, v);
+	}
+	
+	ll res = (ways_down + ways_right) % mod;
+	
+	return dp[i][j][mod_val] = res;
+}
+
+void solve()
+{
+  	ll n, m; cin >> n >> m;
+  	
+  	vvll v(n, vll(m));
+  	
+  	for(ll i = 0; i < n; i++) {
+  		for(ll j = 0; j < m; j++) cin >> v[i][j];
+  	}
+  	
+  	ll k; cin >> k;
+  	
+  	dp.clear();
+    dp.resize(n, vvll(m, vll(k, -1)));
+  	
+  	cout << cnt_paths(0, 0, 0, k, n, m, v);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    
+    // #ifndef ONLINE_JUDGE
+    //      freopen("error.txt", "w", stderr);
+    // #endif
+    
+    int t = 1;
+    // int test = 1;
+    // cin >> t;
+    while(t--) {
+        // cout << "Case #" << test++ << ": ";
+        solve();
+    }
+
+    return 0;
+}
